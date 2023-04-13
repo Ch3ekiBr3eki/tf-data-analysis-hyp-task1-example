c0fdfcc2430b669b2bfb5c4e2b164ec05516bff4
Anton <113473409+Ch3ekiBr3eki@users.noreply.github.com>
2023-04-13 19:19:48 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 323297403 # Ваш chat ID, не меняйте название переменной
 
 def solution(x_success: int, 
              x_cnt: int, 
@@ -11,4 +11,19 @@ def solution(x_success: int,
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    n1 = x_cnt
+    n2 = y_cnt
+    p = (x_success + y_success) / (x_cnt + y_cnt)
+    n1_exp = n1 * p
+    n2_exp = n2 * p
+    
+    chi_sq = ((x_success - n1_exp) ** 2) / n1_exp + ((y_success - n2_exp) ** 2) / n2_exp
+    
+    df = 1
+    
+    critical_value = stats.chi2.ppf(0.98, df)
+    
+    if chi_sq > critical_value:
+        return True
+    else:
+        return False
